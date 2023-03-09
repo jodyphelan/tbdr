6835fab575fddd7e152d3632eed826ebdeb32cd3
Jody Phelan <jody.phelan@lshtm.ac.uk>
2023-03-09 16:35:55 +0000

    update add new result

--- bin/insert_results.py
+++ bin/insert_results.py
@@ -50,12 +50,6 @@ def main(args):
             rows = [{'variant_id': "%(locus_tag)s_%(change)s" % var,'sample_id': data['id']} for var in data['dr_variants'] + data['other_variants']]
             result = conn.execute(insert(sample_variants_table),rows)
 
-    # If a list of samples is supplied through the args object, store it in a list else get the list from looking in the results direcotry
-    if args.samples:
-        samples = [x.rstrip() for x in open(args.samples).readlines()]
-    else:
-        samples = [x.replace(args.suffix,"") for x in os.listdir(args.dir) if x[-len(args.suffix):]==args.suffix]
-
     meta = {}
     for row in csv.DictReader(open(args.metadata_csv)):
         row['id'] = row['wgs_id']
@@ -64,24 +58,20 @@ def main(args):
         if row['country']=="N/A": del row['country'] 
         meta[row['wgs_id']] = row
 
-    # Loop through the sample result files
-    for s in tqdm(samples):
-        # Data has the same structure as the .result.json files
-        data = json.load(open(pp.filecheck("%s/%s%s" % (args.dir,s,args.suffix))))
-        m = meta.get(s)
-        if m:
-            data.update(m)
-        data['public'] = True
-        
-        add_sample(data)
+
+    data = json.load(open(args.json))
+    m = meta.get(data['id'])
+    if m:
+        data.update(m)
+    data['public'] = True
+    
+    add_sample(data)
 
 # Set up the parser
 parser = argparse.ArgumentParser(description='tbprofiler script',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--samples',type=str,help='File with samples')
-parser.add_argument('--dir',default="results/",type=str,help='Directory containing results')
+parser.add_argument('--json',type=str,help='File with samples')
 parser.add_argument('--db',default="tbdb",type=str,help='Database name')
 parser.add_argument('--metadata-csv',type=str,help='Database name',required = True)
-parser.add_argument('--suffix',default=".results.json",type=str,help='File suffix')
 parser.set_defaults(func=main)
 
 args = parser.parse_args()
