0a793dfc8814482e703613e319ba6e6b8b755087
Jody Phelan <jody.phelan@lshtm.ac.uk>
2024-06-16 19:31:27 +0200

    set bcftools as nanopore caller

--- tbdr/worker.py
+++ tbdr/worker.py
@@ -100,6 +100,10 @@ def run_tb_profiler_command(fq1,fq2,uniq_id,platform,result_file_dir):
         if fq1 and fq2:
             sp.call(f"tb-profiler profile --spoligotype --ram 8 --threads 2 --txt --csv -1 {fq1} -2 {fq2} -m {platform.lower()} -p {uniq_id} --dir {result_file_dir}",shell=True, stderr=LOG,stdout=LOG)
         else:
-            sp.call(f"tb-profiler profile --spoligotype --ram 8 --threads 2 --txt --csv -1 {fq1} -m {platform.lower()} -p {uniq_id} --dir {result_file_dir}",shell=True, stderr=LOG,stdout=LOG)
+            if platform == "nanopore":
+                
+                sp.call(f"tb-profiler profile --spoligotype --ram 8 --threads 2 --txt --csv -1 {fq1} --caller bcftools -m {platform.lower()} -p {uniq_id} --dir {result_file_dir}",shell=True, stderr=LOG,stdout=LOG)
+            else:
+                sp.call(f"tb-profiler profile --spoligotype --ram 8 --threads 2 --txt --csv -1 {fq1} -m {platform.lower()} -p {uniq_id} --dir {result_file_dir}",shell=True, stderr=LOG,stdout=LOG)
 
 
